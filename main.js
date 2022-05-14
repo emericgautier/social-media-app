97b8f20e1f7b6674dda8bb9bf03d31f8b9949569
emericgautier <emericgtr@gmail.com>
2022-05-14 10:32:21 +0200

    buit event listener for the form

--- main.js
+++ main.js
@@ -1,4 +1,29 @@
+// form validation | target all the ID selectors from the HTML
 let form = document.getElementById("form");
 let input = document.getElementById("input");
 let msg = document.getElementById("msg");
 let posts = document.getElementById("posts");
+
+// build a submit event listener for the form to prevent the default behaviour
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    console.log("button clicked");
+
+formValidation();
+});
+
+// prevent users from submitting blank input fields
+let formValidation = () => {
+    if (input.value === "") {
+        msg.innerHTML = "Post cannot be blank";
+        console.log("failure");
+    } else {
+        console.log("success");
+        msg.innerHTML = "";
+    }
+};
+
+
+
+
+
